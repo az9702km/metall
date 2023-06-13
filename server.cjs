998a02d079f7ff67c6fb9a0864d1ceb658fecff1
az9702km <az9798mt@gmail.com>
2023-06-13 09:53:00 +0500

    fix: start the app

--- server.cjs
+++ server.cjs
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use(router)
-server.listen(5000, () => {
+server.listen(8000, () => {
   console.log('JSON Server is running')
 })
